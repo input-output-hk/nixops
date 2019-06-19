dba45d750199147f857b14dadbb29811c9baf97d
John Lotoski <john.lotoski@iohk.io>
2019-06-18 20:39:28 -0400

    Adds plugin pattern support
    This commit enables building from release.nix with plugin
    pattern support.  Examples are:
    
    nix-build release.nix -A build.x86_64-linux --arg p "(p: [ p.plugin1 ])"
    
    ./dev-shell --arg p "(p: [ p.plugin1 ])"
    
    See the README.txt and release.nix comments for more detail.

--- data.nix
+++ data.nix
@@ -7,4 +7,18 @@
       version = "0.0.1";
       sha256  = "0dppqqa9ag802mawz635v4b3vmg12dpghwz4dg6q8xh9hx7cycy7";
     };
+  packet =
+    {
+      owner   = "input-output-hk";
+      repo    = "nixops-packet";
+      version = "0.0.1";
+      sha256  = "1qzc15sj45bsvm0f78fqsfdrybbiyil52kffl5zlwd1ccsqi8m3b";
+    };
+  vultr =
+    {
+      owner   = "disassembler";
+      repo    = "nixops-vultr";
+      version = "0.0.1";
+      sha256  = "0404fdcp57a0dkfrs2pcjnq1m6mj53mmca34g2n2jng9lav5wnbr";
+    };
 }
