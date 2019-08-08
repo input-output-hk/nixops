2447be9da55beda18c761896ecc2fb5966dd0447
John Lotoski <john.lotoski@iohk.io>
2019-08-08 12:13:31 -0400

    Adds internal use plugin repos

--- data.nix
+++ data.nix
@@ -14,4 +14,18 @@
       version = "0.0.1";
       sha256  = "1nvdx3p7xb7klb0c9jbfwq2mf8i1hxvwvxxaabjpby5sza8s0qf5";
     };
+  packet =
+    {
+      owner   = "input-output-hk";
+      repo    = "nixops-packet";
+      version = "0.0.4";
+      sha256  = "0zkh41s5l0z6gl8ph35mq7d1ff60hw3b0gy850gk3bsxj4s5j32r";
+    };
+  vultr =
+    {
+      owner   = "disassembler";
+      repo    = "nixops-vultr";
+      version = "0.0.1";
+      sha256  = "0404fdcp57a0dkfrs2pcjnq1m6mj53mmca34g2n2jng9lav5wnbr";
+    };
 }
